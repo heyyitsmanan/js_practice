fd021e1654935dd91883f9619a94842626bd67a1
heyyitsmanan <m.anan.ahuja2203@gmail.com>
2022-01-16 02:45:44 +0530

    Completed all coding challenges for Fundamentals Part 1

--- script.js
+++ script.js
@@ -83,17 +83,87 @@ console.log(' ');
 There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
 
 1. Calculate the average score for each team, using the test data below
+
 2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
 
 3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+
 4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
 
 TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+
 TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+
 TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 
 GOOD LUCK 😀
 */
+
+
+//! Solution Coding Challenge 3
+
+
+console.log(' ');
+console.log("Solution of Coding Challenge 3 Starting...");
+console.log(' ');
+
+//* Scores for 1st round
+
+// const dolphinR1 = 96, dolphinR2 = 108, dolphinR3 = 89;
+// const koalaR1 = 88, koalaR2 = 91, koalaR3 = 110;
+
+//* Scores for 2nd Bonus round
+
+// const dolphinR1 = 97, dolphinR2 = 112, dolphinR3 = 101;
+// const koalaR1 = 109, koalaR2 = 95, koalaR3 = 123;
+
+//* Scores for 3rd Bonus round
+
+const dolphinR1 = 97, dolphinR2 = 112, dolphinR3 = 101;
+const koalaR1 = 109, koalaR2 = 95, koalaR3 = 106;
+
+const dolphinAvgScore = (dolphinR1 + dolphinR2 + dolphinR3) / 3;
+const koalaAvgScore = (koalaR1 + koalaR2 + koalaR3) / 3;
+
+console.log(dolphinAvgScore, koalaAvgScore);
+
+console.log(' ');
+console.log(`Checking the winner using 1st criteria :- `);
+console.log(' ');
+
+if(dolphinAvgScore > koalaAvgScore){
+    console.log(`Dolphin Team Won :D`);
+}
+else if (dolphinAvgScore === koalaAvgScore){
+    console.log(`It's a draw between Dolphin's and Koala's`);
+}
+else{
+    console.log(`Koala Team Won :D`);
+}
+
+console.log(' ');
+console.log(`Checking the winner using 2nd criteria :- `);
+console.log(' ');
+
+const minReq = 100;
+
+if((dolphinAvgScore === koalaAvgScore) && (dolphinAvgScore >= minReq) && (koalaAvgScore > minReq)){
+    console.log(`It's a Draw between both teams :)`);
+}
+else if((dolphinAvgScore > koalaAvgScore) && (dolphinAvgScore >= minReq)){
+    console.log(`Dolphin Team Won :D`);
+}
+else if((koalaAvgScore > dolphinAvgScore) && (koalaAvgScore >= minReq)){
+    console.log(`Koala Team Won :D`);
+}
+else{
+    console.log(`No Team won the cup :(`);
+}
+
+console.log(' ');
+console.log("Solution of Coding Challenge 3 Ended...");
+console.log(' ');
+
 
 ////////////////////////////////////
 // Coding Challenge #4
@@ -111,3 +181,21 @@ HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 
 GOOD LUCK 😀
 */
+
+//! Solution Coding Challenge 4
+
+console.log(' ');
+console.log("Solution of Coding Challenge 4 Starting...");
+console.log(' ');
+
+const billValue = 275;
+// const billValue = 40;
+// const billValue = 430;
+
+const tip = billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.20;
+
+console.log(`The bill was ${billValue} and, the tip was ${tip} and, the total value ${billValue + tip}`);
+
+console.log(' ');
+console.log("Solution of Coding Challenge 4 Ended...");
+console.log(' ');
