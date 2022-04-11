98cd282d5ec76f30d0c54fdfd38a54ea91c9d46b
heyyitsmanan <m.anan.ahuja2203@gmail.com>
2022-04-11 13:15:49 +0530

    Created username function for the Bankist app

--- 6. Arrays/script.js
+++ 6. Arrays/script.js
@@ -61,6 +61,8 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+//* Display Transaction Information
+
 const displayTransactions = function (transactions) {
   containerMovements.innerHTML = "";
   transactions.forEach(function (trans, i) {
@@ -79,6 +81,20 @@ const displayTransactions = function (transactions) {
 };
 
 displayTransactions(account1.movements);
+
+//* Computing Usernames
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+
+createUsernames(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
